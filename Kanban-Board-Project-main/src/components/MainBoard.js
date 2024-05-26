eec9241c59d4c5ab36cc6bf3ad45c84d13854f6e
sakshi mishra <78993444+sakshi8299@users.noreply.github.com>
2024-05-26 22:21:00 +0530

    Update MainBoard.js

--- Kanban-Board-Project-main/src/components/MainBoard.js
+++ Kanban-Board-Project-main/src/components/MainBoard.js
@@ -1,22 +1,21 @@
 import React , {useReducer} from 'react'
-import Priority from './Display/Priority_File'
-import Users from './Display/User_File';
-import Status from './Display/Status_File'
-
+import Status from './Grouping/Status'
+import Priority from './Grouping/Priority'
+import Users from './Grouping/Users';
 
 
 export default function Board({filter , tickets , users}) {
-  const select_user = {};
-  const select_tickets = tickets;
+  const user_s = {};
+  const tick = tickets;
   users.forEach((element, index) => {
-    select_user[element.id] = element;
+    user_s[element.id] = element;
   });
 
   return (
-    <div className='board'>
-      {filter?.grouping === "status" && (<Status filterr={filter} tickets = {select_tickets} users={select_user}/>)}
-      {filter?.grouping === "priority" && (<Priority filterr={filter} tickets = {select_tickets} users = {select_user} />)}
-      {filter?.grouping === "user" && (<Users filterr={filter} tickets = {select_tickets} users = {select_user} />)}
+    <div className='main-board'>
+      {filter?.grouping === "status" && (<Status filterr={filter} tickets = {tick} users={user_s}/>)}
+      {filter?.grouping === "priority" && (<Priority filterr={filter} tickets = {tick} users = {user_s} />)}
+      {filter?.grouping === "user" && (<Users filterr={filter} tickets = {tick} users = {user_s} />)}
     </div>
   )
 }
