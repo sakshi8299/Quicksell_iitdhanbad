56f6671e6601e08d6e3543ed3b65166e9540134d
sakshi mishra <78993444+sakshi8299@users.noreply.github.com>
2024-05-26 22:21:38 +0530

    Update Image.js

--- Kanban-Board-Project-main/src/components/Image.js
+++ Kanban-Board-Project-main/src/components/Image.js
@@ -1,16 +1,18 @@
 import React from "react";
 
-const ProfileImage = ({ name }) => {
-  const hereName = name.split(" ");
-  const name_First = hereName[0] ? hereName[0][0] : "";
-  const name_Last = hereName[1] ? hereName[1][0] : "";
+const ProfileImage = ({ name , available}) => {
+  const nameParts = name.split(" ");
+  const firstNameInitial = nameParts[0] ? nameParts[0][0] : "";
+  const lastNameInitial = nameParts[1] ? nameParts[1][0] : "";
 
   return (
     <div>
-        <span style={{color:'#f2ebeb'}} className="profile-image">
-            {name_First}
-            {name_Last}
-        </span>    
+        <span style={{color:'#ffffff'}} className="user-profile-image">
+            {firstNameInitial}
+            {lastNameInitial}
+        </span>
+        {available && (<span className="available" style={{fontSize : '2rem' , color: '#ffc400'}}>•</span>)}
+        
     </div>
   );
 };
